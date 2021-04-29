8ba81d0ba8456712c39f689d72fcb07ebbd991c0
Adam Hamar <adam.hamar@gmail.com>
2021-04-29 10:27:39 +0200

    Implement the AddDigits class

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,23 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+    private final static int ASCII_CODE_OF_0 = 48;
+    
+    public int addDigits(String input) {
+        if (input == null || input.trim().equals("")) {
+            return -1;
+        }
+        
+        int sum = 0;
+        
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            
+            if (currentChar >= '0' && currentChar <= '9') {
+                sum += currentChar - ASCII_CODE_OF_0;
+            }
+        }
+        
+        return sum;
+    }
 }
